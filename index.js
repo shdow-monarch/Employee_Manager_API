6366815a3eb79e6dac386c6c0ee6746620804ace
shdow-monarch <invincibleshadowmonarch@gmail.com>
2022-08-27 13:31:56 +0530

    installed cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const koa = require("koa");
+const cors = require("cors");
 const router = require("./routes");
 require("./config");
 
 const app = new koa();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => console.log("Server started"));
